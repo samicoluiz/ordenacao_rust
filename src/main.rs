0fc82ae2dfc61a7bb1551af432bf5242f5b571fa
samico <loouiz@gmail.com>
2023-07-11 16:49:09 -0300

    Insertion sort, selection sort e bubble sort implementados e nao testados

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,20 @@ use std::io;
 use rand::prelude::*;
 
 
-fn bubblesort(){}
+fn bubblesort(arr: Vec<i32>) -> Vec<i32>{
+    let mut ordenado = arr;
+    let mut trocas = true;
+    while trocas {
+        trocas = false;
+        for i in 0..ordenado.len() - 1 {
+            if ordenado[i] > ordenado[i + 1] {
+                ordenado.swap(i, i + 1);
+                trocas = true;
+            }
+        }
+    }
+    return ordenado;
+}
 
 fn quick_sort(arr: Vec<i32>) -> Vec<i32> {
     if arr.len() < 2 {
@@ -13,9 +26,37 @@ fn quick_sort(arr: Vec<i32>) -> Vec<i32> {
     return [quick_sort(menores), vec![pivo], quick_sort(maiores)].concat();
     }
 
-fn insertsort(){}
+fn insertsort(arr: Vec<i32>) Vec<i32> -{
+    let mut ordenado = Vec::new();
+    for i in arr {
+        let mut inserido = false;
+        for j in 0..ordenado.len() {
+            if i < ordenado[j] {
+                ordenado.insert(j, i);
+                inserido = true;
+                break;
+            }
+        }
+        if !inserido {
+            ordenado.push(i);
+        }
+    }
+    return ordenado;
+}
 
-fn selection_sort(){}
+fn selection_sort(Vec <i32>) -> Vec<i32> {
+    let mut ordenado = arr;
+    for i in 0..ordenado.len() - 1 {
+        let mut menor = i;
+        for j in i + 1..ordenado.len() {
+            if ordenado[j] < ordenado[menor] {
+                menor = j;
+            }
+        }
+        ordenado.swap(i, menor);
+    }
+    return ordenado;
+}
 
 fn merge_sort(){}
 
